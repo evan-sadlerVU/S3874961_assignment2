c2fe1e70a8c6f8fa908b74035d20c0fb555957f8
ertplus <evansadler93@gmail.com>
2025-10-16 09:03:26 +1100

    Added Basic Material3 login layout. Added S3874961Assignment2App.kt application class and basic DI setup.

---
+++ app/src/main/java/com/example/s3874961_assignment2/S3874961Assignment2App.kt
@@ -0,0 +1,8 @@
+package com.example.s3874961_assignment2
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class S3874961Assignment2App: Application() {
+}
